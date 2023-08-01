1b2a59b3d1f23b6118c9adab3308ad7206bae0c2
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-01 11:31:59 +0300

    Reducer: add todolist-reducer test action creator addTodolistAC

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,4 +1,4 @@
-import {ChangeTodolistFilterActionType, removeTodolistAC, todolistsReducer} from './todolists-reducer'
+import {addTodolistAC, ChangeTodolistFilterActionType, removeTodolistAC, todolistsReducer} from './todolists-reducer'
 import { v1 } from 'uuid'
 import {FilterValueType, TodolistType} from '../App'
 
@@ -28,7 +28,7 @@ test('correct todolist should be added', () => {
         {id: todolistId2, title: 'What to buy', filter: 'all'}
     ]
 
-    const endState = todolistsReducer(startState, {type: 'ADD-TODOLIST', title: newTodolistTitle})
+    const endState = todolistsReducer(startState,addTodolistAC(newTodolistTitle))
 
     expect(endState.length).toBe(3)
     expect(endState[2].title).toBe(newTodolistTitle)
