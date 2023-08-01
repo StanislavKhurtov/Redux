b4fe94cdd4a7356f283873fd04927c7dde83bab7
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-01 10:44:09 +0300

    Reducer: add todolist case 'CHANGE-TODOLIST-FILTER'

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,6 +1,6 @@
 import { todolistsReducer } from './todolists-reducer'
 import { v1 } from 'uuid'
-import { TodolistType } from '../App'
+import {FilterValueType, TodolistType} from '../App'
 
 test('correct todolist should be removed', () => {
     let todolistId1 = v1()
@@ -63,7 +63,7 @@ test('correct filter of todolist should be changed', () => {
     let todolistId1 = v1()
     let todolistId2 = v1()
 
-    let newFilter: FilterValuesType = 'completed'
+    let newFilter: FilterValueType = 'completed'
 
     const startState: Array<TodolistType> = [
         {id: todolistId1, title: 'What to learn', filter: 'all'},
