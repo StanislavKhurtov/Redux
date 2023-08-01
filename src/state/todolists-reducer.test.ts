db7bce483683403d286262f495b927873b10b004
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-01 10:09:57 +0300

    Reducer: add todolist test 'correct todolist should be added'

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -18,3 +18,19 @@ test('correct todolist should be removed', () => {
 })
 
 
+test('correct todolist should be added', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let newTodolistTitle = 'New Todolist'
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    const endState = todolistsReducer(startState, {type: 'ADD-TODOLIST', title: newTodolistTitle})
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
+})
