430d2f51b9f2d7ac9fe71a08ca94b5a80b89efea
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-07 18:49:20 +0300

    refactor: lesson10 change structure index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {AppWithRedux} from "./AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "./state/store";
+import {AppWithReducers} from "./AppWithReducers";
 
 
-ReactDOM.render(
-    <Provider store={store}>
-        <AppWithRedux/>
-    </Provider>,
-    document.getElementById('root'));
+
+ReactDOM.render(<AppWithReducers />, document.getElementById('root'));
